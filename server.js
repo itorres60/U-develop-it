59778e24cd672ac0b23c4f93130fd2be99425710
J <torres.j.isaias@gmail.com>
2021-09-07 15:14:22 -0500

    connected node.js server to sql database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,12 +8,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: 'Heart4721.',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
 
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello World'
-  });
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows)
 });
 
 // Default response for any other request (Not Found)
